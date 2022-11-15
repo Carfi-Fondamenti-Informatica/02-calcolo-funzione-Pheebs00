3fc0cbb82d17ea8de7ab00fddae62e20599a959a
Pheebs00 <115625720+Pheebs00@users.noreply.github.com>
2022-11-15 16:20:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 int main(){
     float a=0,b=0, x=0, y=0, z=0;
-    cout << "Inserire numeri razzionali \n";
     cin >> a >> b >> x >> y;
     if(x<0&&y>0){
         z = (a*x)-(b*y);
@@ -12,7 +11,7 @@ int main(){
     }
     else {
         z = a*b+b*y*y;
-        cout <<"\nz="<<z<< endl;
+        cout << "z=" << z << endl;
     }
     return 0;
 }
